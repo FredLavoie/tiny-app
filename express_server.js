8dd04d6e7f149135c363b5881e4d690e1e2ba292
FredLavoie <flavoie3@gmail.com>
2019-06-04 21:57:05 +0000

    add urls_new page

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 let app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 
@@ -31,10 +34,20 @@ app.get("/urls", (request, response) => {
   response.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (request, response) => {
   let templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
-  // console.log(request.params.shortURL);
-  // console.log(urlDatabase[request.params.shortURL]);
-
   response.render("urls_show", templateVars);
 });
+
+app.post("/urls", (request, response) => {
+  console.log(request.body);  // Log the POST request body to the console
+  response.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  return (Math.random() * 6).toString(64).substring(6);
+}
