b31fad69d5a20cf0a7cb0e23fb1c227728affa9d
FredLavoie <flavoie3@gmail.com>
2019-06-04 20:50:48 +0000

    add urls_show page and update server file

--- express_server.js
+++ express_server.js
@@ -5,17 +5,17 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
-let urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (request, response) => {
-  response.send("Hello!");
-});
-
 app.listen(PORT, () => {
   console.log(`tiny-app listening on port ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  response.send("Hello!");
 });
 
 app.get("/urls.json", (request, response) => {
@@ -26,10 +26,15 @@ app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-
 app.get("/urls", (request, response) => {
-  let templateVars = { urls: urlDatabase };
-  console.log(templateVars);
-  
+  let templateVars = { urls: urlDatabase };  
   response.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (request, response) => {
+  let templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
+  // console.log(request.params.shortURL);
+  // console.log(urlDatabase[request.params.shortURL]);
+
+  response.render("urls_show", templateVars);
 });
