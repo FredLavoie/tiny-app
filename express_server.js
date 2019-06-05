df96b67ec47c2dd94611cb5e1df065a67f62451f
FredLavoie <flavoie3@gmail.com>
2019-06-05 18:17:45 +0000

    Update styling of various buttons and refactor code for readability

--- express_server.js
+++ express_server.js
@@ -1,21 +1,31 @@
-const express = require("express");
-let app = express(); // app is the server
-const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser"); // makes post request readable
+/************************************* REQUIRED PACKAGES / PORT ****************************************/
+/*******************************************************************************************************/
 
+const express = require("express");
+const bodyParser = require("body-parser"); // makes post request readable
+const PORT = 8080; // default port 8080
+
+let app = express(); // app is the server
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// database of URL that the user has as short URLs
+/********************************************* DATABASE ************************************************/
+/*******************************************************************************************************/
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// when server is initialisted, it listens for requests
+/****************************************** SERVER - LISTEN ********************************************/
+/*******************************************************************************************************/
+
 app.listen(PORT, () => {
   console.log(`tiny-app listening on port ${PORT}`);
 });
+
+/******************************************** SERVER - GET *********************************************/
+/*******************************************************************************************************/
 
 // get requests for urls.json will provide the urlDatabase as a JSON file
 app.get("/urls.json", (request, response) => {
@@ -33,6 +43,12 @@ app.get("/urls/new", (request, response) => {
   response.render("urls_new");
 });
 
+// redirect traffic of u/:shortURL to longURL
+app.get("/u/:shortURL", (request, response) => {
+  const longURL = urlDatabase[request.params.shortURL];
+  response.redirect(longURL);
+});
+
 // adds shorturl and longurl to 'urlDatabase' object on submit
 app.get("/urls/:shortURL", (request, response) => {
   let templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
@@ -44,16 +60,10 @@ app.post("/urls", (request, response) => {
   let item = generateRandomString();
   urlDatabase[item] = request.body.longURL;
   response.redirect("urls/" + item);
-
 });
 
-/****************  not sure what this app.get is doing  *****************/
-/************************************************************************/
-app.get("/u/:shortURL", (request, response) => {
-  const longURL = request.params.shortURL;
-  // response.redirect(urlDatabase[longURL]);
-  response.redirect("urls_show",longURL);
-});
+/******************************************** SERVER - POST ********************************************/
+/*******************************************************************************************************/
 
 // delete entry from urlDatabase object
 app.post("/urls/:shortURL/delete", (request, response) => {
@@ -71,6 +81,9 @@ app.post("/login", (request, response) => {
   response.cookie('username', { username: request.body.username });
   response.redirect("/urls");
 });
+
+/********************************************** FUNCTIONS **********************************************/
+/*******************************************************************************************************/
 
 function generateRandomString() {
   return (Math.random() * 6).toString(36).substring(6);
